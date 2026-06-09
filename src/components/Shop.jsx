05983041bc3410220d6201881f2ac1854e86f2db
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-06-09 18:39:52 +0200

    added api fetching and api products rendering

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,79 +1,92 @@
+import { useState, useEffect } from 'react';
+
 import styles from '../style/Shop.module.css';
 
-import productImage from "../assets/product-card-default.svg"
+import productImage from '../assets/product-card-default.svg';
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((response) => setProducts(response))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className={styles.shop}>
       <Banner />
       <Filters />
-      <ProductList />
+      <ProductList products={products} />
     </div>
   );
 };
 export default Shop;
 
 const Banner = () => {
-    return(
-        <div className={styles.banner}>
-            <h2>All Products</h2>
-        </div>
-    );
-}
+  return (
+    <div className={styles.banner}>
+      <h2>All Products</h2>
+    </div>
+  );
+};
 
 const Filters = () => {
-    return(
-        <div className={styles.filters}>
-            <h3>Filters</h3>
-            <hr />
-            <h4>Price</h4>
-            <hr />
-            <h4>Categories</h4>
-            <hr />
-            <h4>Color</h4>
-            <hr />
-        </div>
-    );
-}
+  return (
+    <div className={styles.filters}>
+      <h3>Filters</h3>
+      <hr />
+      <h4>Price</h4>
+      <hr />
+      <h4>Categories</h4>
+      <hr />
+      <h4>Color</h4>
+      <hr />
+    </div>
+  );
+};
 
-const ProductList = () => {
-    return(
-        <div className={styles.productList}>
-            <h3>nº items</h3>
-            <hr />
-            <div className={styles.productsDisplay}>
-                <ProductCard /> 
-                <ProductCard /> 
-                <ProductCard /> 
-            </div>
-        </div>
-    );
-}
+const ProductList = (products) => {
+  return (
+    <div className={styles.productList}>
+      <h3>nº items</h3>
+      <hr />
+      <div className={styles.productsDisplay}>
+        {products.products.map((product) => (
+          <ProductCard 
+            key={product.title}
+            title={product.title}
+            image={product.image}
+            price={product.price}
+        />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const ProductCard = () => {
-    return(
-        <div className={styles.productCard}>
-            <img src={productImage} alt="product image" />
-            <hr />
-            <div className={styles.productCardText}>
-                <div className={styles.productCardInfo}>
-                    <p>Basic Tee</p>
-                    <div className={styles.productCardInfoSizes}>
-                        <p>Sizes:</p>
-                        <ul>
-                            <li>XS</li>
-                            <li>S</li>
-                            <li>M</li>
-                            <li>L</li>
-                            <li>XL</li>
-                        </ul>   
-                    </div>
-                </div>
-                <div className={styles.productCardPrice}>
-                    34.95€
-                </div>
-            </div>  
+const ProductCard = (props) => {
+  return (
+    <div className={styles.productCard}>
+      <img src={props.image} alt={props.title} className={styles.productCardImg}/>
+      <hr />
+      <div className={styles.productCardText}>
+        <div className={styles.productCardInfo}>
+          <p>{props.title}</p>
+          <div className={styles.productCardInfoSizes}>
+            <p>Sizes:</p>
+            <ul>
+              <li>XS</li>
+              <li>S</li>
+              <li>M</li>
+              <li>L</li>
+              <li>XL</li>
+            </ul>
+          </div>
         </div>
-    );
-}
-
+        <div className={styles.productCardPrice}>{props.price}€</div>
+      </div>
+    </div>
+  );
+};
